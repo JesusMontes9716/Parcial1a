6c58e825a507779ca739fa15e9b4d5fba4c2bc28
JesusMontes <za210110199@zapopan.tecmm.edu.mx>
2023-02-16 13:33:49 -0600

    se agregaron nuevos temas

--- FUNDAMENTOS.py
+++ FUNDAMENTOS.py
@@ -111,9 +111,45 @@ print(t)
 print(t[2])
 print("t[0:2]:", t[0:2])
 # t[1] = 34 operacion no valida en tuplas, no se puede utilizar 
+print('-----------------------------------------------------------------------------------------------------')
 
+nuevoTema("Conjuntos")
+t = {50, 20, 30, 40, 10, 50}
+print("t = ", t)
+print(type(t))
 
+print('-----------------------------------------------------------------------------------------------------')
 
+nuevoTema("Diccionarios")
+d = {1:"valor1","valor2":2j}
+print(d,type(d))
+##print("d[valor2]:",d"[valor2]")
+
+print('-----------------------------------------------------------------------------------------------------')
+
+nuevoTema("Cadenas")
+cadena1 = "cadena con doble comilla"
+cadena2 = 'cadena con comillas simples'
+cadena3 = '''esta es una cadena 
+de varias lineas con tabulares y
+saltos'''
+print(cadena1, type(cadena1))
+print(cadena2, type(cadena2))
+print(cadena3, type(cadena3))
+print("segmentacion de cadenas")
+print(cadena1[5:11])
+print(cadena1[:5])
+print(cadena1[7:]) 
+print(cadena1[-8:-1])
+print(cadena1[0:18:1])
+print(cadena1[0:18:2])
+print(cadena1[0:18:3])
+
+cadena1 = "hola"
+cadena4 = (cadena1 + " ")*5
+print(cadena4)
+cadena5 = cadena4.capitalize()
+print(cadena5)
 
 
 print('-----------------------------------------------------------------------------------------------------')
