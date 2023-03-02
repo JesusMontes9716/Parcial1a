576d91dca3fe968bd8f64e329915b84e98ac188c
JesusMontes <za210110199@zapopan.tecmm.edu.mx>
2023-03-02 14:54:57 -0600

    se actualizo

--- INTERFACES/CONVERSOR.py
+++ INTERFACES/CONVERSOR.py
@@ -12,6 +12,34 @@ class Conversor:
         self.pies = StringVar()
         self.metros = StringVar()
 
+        mainFrame = ttk.Frame(raiz)
+        mainFrame.grid(column = 0, row = 0 )
+
+        piesEntry = ttk.Entry(mainFrame, textvariable= self.pies)
+        piesEntry.grid(column=1,row=0)
+
+        ttk.Label(mainFrame, text="Pies").grid(column=2, row=0)
+        ttk.Label(mainFrame, text="Son equivalentes a").grid(column=0, row=1)
+        ttk.Label(mainFrame, textvariable=self.metros).grid(column=1, row=1)
+        ttk.Label(mainFrame, text="Metros").grid(column=2, row=1)
+
+        ttk.Button(mainFrame, text="Calcular", command=self.calcular).grid(column=2, row=2)
+
+        #Hacer operacion precionando Enter 
+        raiz.bind("<Return>", self.calcular)
+
+    def calcular(self, *args):
+
+        print("Boton Precionado")
+        piesUsuario = self.pies.get() #Siempre devuelve una cadena
+        print("Pies ingresados: ", piesUsuario)
+        piesFlotante = float(piesUsuario) #Conversion de cadena a flotante 
+        metros = piesFlotante * 0.3048
+        print("Metros: ", metros)
+        self.metros.set(metros)
+        
+
+
 if __name__=="__main__":
     print("Este es el archivo Principal.")
     print("Nada mas se mostrara esto si es el Pricipal")
