aad2158b473ea794e1cda9337b2db096b7bfb56b
JesusMontes <za210110199@zapopan.tecmm.edu.mx>
2023-03-07 19:42:22 -0600

    actualizacion de archivos

--- INTERFACES/act1.py
+++ INTERFACES/act1.py
@@ -3,10 +3,10 @@ from tkinter import ttk
 
 class act1: 
     #tipo de constructor
-    def _init_(self, raiz):
-      raiz.title("INICIO DE SECION")
+    def __init__(self, raiz):
+      raiz.title("login")
 
-      self.usuario='USUARIO '
+      self.usuario='usuario'
       self.contraseña='1234'
      
       mainFrame = ttk.Frame(raiz , padding="3 3 12 12")
@@ -20,8 +20,4 @@ class act1:
       ttk.Label(mainFrame, textvariable=self.contraseña).grid(column=1,row=1)
       ttk.Label(mainFrame, text="Contraseña ").grid(column=0,row=3)
       ttk.Label(mainFrame).grid(column=0,row=4)
-      ttk.Button(mainFrame, text="Ingresar",command=self.contraseña).grid(column=1,row=5,sticky=(E))
-
-if __name__=="__main__":
-  print("Este es el archivo Principal.")
-  print("Nada mas se mostrara esto si es el Pricipal")
+      ttk.Button(mainFrame, text="Ingresar",command=self.contraseña).grid(column=1,row=5, sticky=(E))
