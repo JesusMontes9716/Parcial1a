6c8145d538efb0f05ab916313293eb33575ef5ce
JesusMontes <za210110199@zapopan.tecmm.edu.mx>
2023-03-09 21:51:05 -0600

    ACTUALIZACION

--- INTERFACES/act2.py
+++ INTERFACES/act2.py
@@ -1,73 +1,64 @@
-from tkinter import *  
+from tkinter import*
 from tkinter import ttk
 
 raiz = Tk()
-self = Tk
 
-raiz.title("login")
 estado = StringVar()
-self.usuario='usuario'
-self.contraseña='1234'
-
-mainFrame = ttk.Frame(raiz , padding="30 30 40 40")
+mainFrame = ttk.Frame(raiz, padding="10 10 10 10")#izquierda arriba derecha abajo
 mainFrame.grid(column=0, row=0)
 
-Frame1 = ttk.Frame(mainFrame,borderwidth=3,relief="raised")
-Frame1.grid(column=0,row=0)
+frame1 = ttk.Frame(mainFrame, padding="10 10 10 10", borderwidth=3, relief="raised")
+frame1.grid(column=0, row=0)
 
-Frame2 = ttk.Frame(mainFrame,borderwidth=3,relief="raised")
-Frame2.grid(column=2,row=2)
+frame2 = ttk.Frame(mainFrame, padding="10 10 10 10", borderwidth=3, relief="raised")
+frame2.grid(column=0, row=1)
+frame3 = ttk.Frame(mainFrame, padding="10 10 10 10")
+frame3.grid(column=1, row=0)
+        
+nombreEntry = ttk.Entry(frame1, width=30)
+nombreEntry.grid(column=1, row=0, pady=12, sticky=(W, E))
 
-usuarioEntry = ttk.Entry(Frame1, width=30, textvariable=self.usuario)
-usuarioEntry.grid(column=1, row=0)
+aPaternoEntry = ttk.Entry(frame1, width=30)
+aPaternoEntry.grid(column=1, row=2, pady=12, sticky=(W, E))
 
-ttk.Label(Frame1, text="NOMBRE").grid(column=0,row=0)
-usuarioEntry = ttk.Entry(Frame1, width=30, textvariable=self.usuario)
-usuarioEntry.grid(column=1, row=3)
-ttk.Label(Frame1, textvariable=self.contraseña).grid(column=1,row=1)
+aMaternoEntry = ttk.Entry(frame1, width=30)
+aMaternoEntry.grid(column=1, row=3, pady=12, sticky=(W, E))
 
-ttk.Label(Frame1, text="AP.PATERNO ").grid(column=0,row=3)
-usuarioEntry = ttk.Entry(mainFrame, width=30, textvariable=self.usuario)
-ttk.Label(Frame1, textvariable=self.contraseña).grid(column=0,row=4)
+correoEntry = ttk.Entry(frame1, width=30)
+correoEntry.grid(column=1, row=4, pady=12, sticky=(W, E))
 
+movilEntry = ttk.Entry(frame1, width=30)
+movilEntry.grid(column=1, row=5, pady=12, sticky=(W, E))
+        
+ttk.Label(frame1, text="NOMBRE:  ").grid(column=0, row=0, pady=12, sticky=(E))
+ttk.Label(frame1, text="AP. PATERNO:  ").grid(column=0, row=2, pady=12, sticky=(E))
+ttk.Label(frame1, text="AP. MATERNO:  ").grid(column=0, row=3, pady=12, sticky=(E))
+ttk.Label(frame1, text="CORREO:  ").grid(column=0, row=4, pady=12, sticky=(E))
+ttk.Label(frame1, text="MOVIL:  ").grid(column=0, row=5, pady=12, sticky=(E))
 
-ttk.Label(Frame1, text="AP.MATERNO ").grid(column=0,row=5)
-usuarioEntry = ttk.Entry(Frame1, width=30, textvariable=self.usuario)
-usuarioEntry.grid(column=1, row=5)
-ttk.Label(Frame1, textvariable=self.contraseña).grid(column=0,row=6)
+ttk.Label(frame2, text="Aficiones:  ").grid(column=0, row=0, pady=12, sticky=(E))
+leer = ttk.Checkbutton(frame2, text='LEER')
+leer.grid(column=0, row=1, sticky=(W))
+musica = ttk.Checkbutton(frame2, text='MUSICA')
+musica.grid(column=1, row=1, sticky=(W))
+videojuegos = ttk.Checkbutton(frame2, text='VIDEOJUEGOS')
+videojuegos.grid(column=2, row=1, sticky=(W))
 
-ttk.Label(Frame1, text="CORREO ").grid(column=0,row=7)
-usuarioEntry = ttk.Entry(Frame1, width=30, textvariable=self.usuario)
-usuarioEntry.grid(column=1, row=7)
-ttk.Label(Frame1, textvariable=self.contraseña).grid(column=0,row=8)
+        
+comboEstados = ttk.Combobox(mainFrame, textvariable=estado)
+comboEstados.grid(column=1, row=1, pady=50)
+comboEstados['values']=("Estados 32")
+        
+ttk.Button(mainFrame, text="GUARDAR",).grid(column=0, row=5, pady=12, sticky=(W))
+ttk.Button(mainFrame, text="CANCELAR",).grid(column=0, row=5, pady=12, sticky=(N))
 
-ttk.Label(Frame1, text="MOVIL ").grid(column=0,row=9)
-usuarioEntry = ttk.Entry(Frame1, width=30, textvariable=self.usuario)
-usuarioEntry.grid(column=1, row=9)
-ttk.Label(Frame1, textvariable=self.contraseña).grid(column=0,row=10)
+estudiante = ttk.Radiobutton(frame3, text='ESTUDIANTE')
+estudiante.grid(column=0, row=0, sticky=(W))
+empleado = ttk.Radiobutton(frame3, text='EMPLEADO')
+empleado.grid(column=0, row=1, sticky=(W))
+desempleado = ttk.Radiobutton(frame3, text='DESEMPLEADO')
+desempleado.grid(column=0, row=2, sticky=(W))
 
-ttk.Button(mainFrame, text="GUARDAR",command=self.contraseña).grid(column=0,row=20, sticky=(W))
-ttk.Button(mainFrame, text="CANCELAR",command=self.contraseña).grid(column=1,row=20, sticky=(W))
-
-
-
-comboEstados = ttk.Combobox(raiz, textvariable=estado).grid(column=1,row=20, sticky=(W))
-#comboEstados.grid()
-#comboEstados['values'] = ('Jalisco', 'Nayarit', 'Colima', 'Michoacan')
-
-
-
-chkBoton = ttk.Checkbutton(Frame2, raiz, text="LEER", command= self).grid(column=0,row=0)
-chkBoton = ttk.Checkbutton(Frame2,raiz, text="MUSICA", command= self).grid(column=1,row=1)
-chkBoton = ttk.Checkbutton(Frame2, raiz, text="VIDEOJUEGOS", command= self).grid(column=2,row=2)
-
-
-home = ttk.Radiobutton(raiz, text='Estudiante').grid(column=4, row=0, sticky=(W))
-home = ttk.Radiobutton(raiz, text='Empleado').grid(column=4, row=1, sticky=(W))
-home = ttk.Radiobutton(raiz, text='Desempleado').grid(column=4, row=2, sticky=(W))
-
-
-
-
+nombreEntry.focus()
 
 raiz.mainloop()
